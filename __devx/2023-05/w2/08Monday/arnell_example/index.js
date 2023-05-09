d94082157364e2b39a3e4e2e398a6145b93fdec9
Chris Dahlin <chrisdahlin@gmail.com>
2023-05-09 18:50:49 -0400

    Monday 5/8/23 night commit

---
+++ __devx/2023-05/w2/08Monday/arnell_example/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Use the body-parser middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve the index.html file when the user navigates to the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Handle form submissions
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  res.send(`Welcome, ${name}!`);
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
