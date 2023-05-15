76705142c93ad179f0a82d4731bd6c023fc51e6d
Chris Dahlin <chrisdahlin@gmail.com>
2023-05-15 19:39:27 -0400

    Pedro And Gale Tutotial
    Monday Night commit for React Tutorials

---
+++ __devx/2023-05/w2/11Thursday/pedro_tutorial/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+let age = 10;
+let name = age  > 10 ? "Pedro" : "Jack";
+
+
+const Component = () => {
+
+return age > 10 ? <div>Pedro</div> : <div>Jack</div>;
+
+}
+function App() {
+  return (
+    <div className="App">
+
+
+    </div>
+
+  )}
+export default App;
