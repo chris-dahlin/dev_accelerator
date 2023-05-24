f87c3fbcc9a91ac264dcad52118b169b296f6550
Chris Dahlin <chrisdahlin@gmail.com>
2023-05-23 20:11:29 -0400

    Using Const to add HTML in React.DOM

--- __devx/2023-05/w4/23Tuesday/index.js
+++ __devx/2023-05/w4/23Tuesday/index.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+// ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+
+// ReactDOM.render(<p>Hi, my name is Chris</p>, document.getElementById("root"));
+const page = (
+  <div>
+    <h1 className="header">This is JSX</h1>
+    <p>This is a paragraph</p>
+  </div>
+);
+
+console.log(page);
+
+ReactDOM.render(page, document.getElementById("root"));
