0a421ed21d39f01720eecb82b26ffa68cbb68807
Chris Dahlin <chrisdahlin@gmail.com>
2023-11-20 19:50:59 -0500

    Project 0 - tictactoe progress

--- __devx/CS_50/CS50_AI/Project0/tictactoe/tictactoe.py
+++ __devx/CS_50/CS50_AI/Project0/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -55,21 +56,67 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    
+    if action not in actions(board):
+        raise Exception("Not Valid move")
+    
+    row, col = action
+    board_copy = copy.deepcopy(board)
+    board_copy[row][col] = player(board)
+    return board_copy
 
+def checkRow(board, player):
+    for row in range(len(board)):
+        if board[row][0] == player and board[row][1] == player and board[row][2] == player:
+            return True
+    return False
+
+def checkCol(board, player):
+    for col in range(len(board[0])):
+        if board[0][col] == player and board[1][col] == player and board[2][col] == player:
+            return True
+    return False
+
+def checkFirstDiag(board, player):
+    count = 0
+    for row in range(len(board)):
+       for col in range(len(board[0])):
+            if row == col and board[row][col] == player:
+                count += 1
+    if count == 3:
+        return True
+    else: 
+        return False 
+    
+def checkSecondDiag(board, player):
+    count = 0
+    for row in range(len(board)):
+        for col in range(len(board)):
+            if (len(board) - row - 1) == col and board[row][col] == player:
+                count +=1
+    if count == 3:
+        return True
+    else:
+        return False
+    
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
-
+    if checkRow(board, X) or checkCol(board, X) or checkFirstDiag(board, X) or checkSecondDiag(board, X):
+        return X
+    elif checkRow(board, O) or checkCol(board, O) or checkFirstDiag(board, O) or checkSecondDiag(board, O):
+        return O
+    else:
+        return None
+    
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+
 
 
 def utility(board):
