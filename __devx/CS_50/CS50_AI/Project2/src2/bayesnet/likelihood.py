6f40686d1bf4b072334574a96dedcf5e724fa156
Chris Dahlin <chrisdahlin@gmail.com>
2023-11-27 19:03:27 -0500

    Likelihood -example work

--- __devx/CS_50/CS50_AI/Project2/src2/bayesnet/likelihood.py
+++ __devx/CS_50/CS50_AI/Project2/src2/bayesnet/likelihood.py
@@ -1,6 +1,8 @@
 from model import model
 
 # Calculate probability for a given observation
-probability = model.probability([["none", "no", "on time", "attend"]])
-
+# probability = model.probability([["none", "no", "on time", "attend"]])
+# 0.34019999999999995
+probability = model.probability([["none", "no", "on time", "miss"]])
+# 0.037800000000000014
 print(probability)
