0a90f51589a625426a6f9f37126898c54789bca5
Chris Dahlin <chrisdahlin@gmail.com>
2023-11-27 19:09:27 -0500

    Inference - example work

--- __devx/CS_50/CS50_AI/Project2/src2/bayesnet/inference.py
+++ __devx/CS_50/CS50_AI/Project2/src2/bayesnet/inference.py
@@ -2,6 +2,7 @@ from model import model
 
 # Calculate predictions
 predictions = model.predict_proba({
+    "rain": "heavy",
     "train": "delayed"
 })
 
@@ -13,3 +14,28 @@ for node, prediction in zip(model.states, predictions):
         print(f"{node.name}")
         for value, probability in prediction.parameters[0].items():
             print(f"    {value}: {probability:.4f}")
+
+
+
+# first prompt
+# rain
+#     none: 0.4583
+#     light: 0.3069
+#     heavy: 0.2348
+# maintenance
+#     yes: 0.3568
+#     no: 0.6432
+# train: delayed
+# appointment
+#     attend: 0.6000
+#     miss: 0.4000
+
+# second prompt
+# rain: heavy
+# maintenance
+#     yes: 0.1176
+#     no: 0.8824
+# train: delayed
+# appointment
+#     attend: 0.6000
+#     miss: 0.4000
