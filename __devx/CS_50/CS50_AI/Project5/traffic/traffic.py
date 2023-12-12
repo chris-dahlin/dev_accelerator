1b0e0023be2026ec3f578ac7291eced9ad35391b
Chris Dahlin <chrisdahlin@gmail.com>
2023-12-12 16:36:47 -0500

    Project 5 - Traffic Completed

--- __devx/CS_50/CS50_AI/Project5/traffic/traffic.py
+++ __devx/CS_50/CS50_AI/Project5/traffic/traffic.py
@@ -77,10 +77,6 @@ def load_data(data_dir):
 
     return (images, labels)
 
-
-
-
-               
         # for filename in os.listdir(category_dir):
         #     image = cv2.imread(os.path.join(category_dir, filename))
         #     image = cv2.resize(image, (IMG_WIDTH, IMG_HEIGHT))
@@ -94,8 +90,37 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
 
+    # Convolutional layer. Learn 32 filters using a 3x3 kernel
+    tf.keras.layers.Conv2D(
+        32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+    ),
+
+    # Max-pooling layer, using 2x2 pool size
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+    # Flatten units
+    tf.keras.layers.Flatten(),
+
+    # Add a hidden layer with dropout
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dropout(0.5),
+
+    # Add an output layer
+    tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 if __name__ == "__main__":
     main()
